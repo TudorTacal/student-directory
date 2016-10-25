574b1676a6b69148a255fda20869937321f6fb92
TudorTacal <tudortacal@gmail.com>
2016-10-25 08:38:55 +0000

    Added the input method

--- directory.rb
+++ directory.rb
@@ -1,18 +1,19 @@
-#let's put all the students into an array
-students=[
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge",cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator",cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-    ]
-
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students=[]
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
@@ -26,7 +27,8 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"  
 end
-#nothing happens untul we call this methods
+#nothing happens until we call this methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
