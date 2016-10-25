8fe428a9a3e19becf92a5081c4aa2262623fa0cc
TudorTacal <tudortacal@gmail.com>
2016-10-25 11:18:54 +0000

    Exercise 6, researched the center method and aligned the output

--- directory.rb
+++ directory.rb
@@ -3,29 +3,13 @@ def input_students
   puts "To finish, just hit return twice"
   #create an empty array
   students=[]
-  puts "Insert student name:"
   name = gets.chomp
-  puts "Insert student hobby:"
-  hobby = gets.chomp
-  puts "Insert student country of birth:"
-  country = gets.chomp
-  puts "Insert student height:"
-  height = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
-    
     #add the student hash to the array
-    students << {name: name, hobby: hobby, country: country, height: height, cohort: :november}
+    students << {name: name,  cohort: :november}
     puts "Now we have #{students.count} students"
-    puts "Insert student name:"
     name = gets.chomp
-    break if name.empty?
-    puts "Insert student hobby:"
-    hobby = gets.chomp
-    puts "Insert student country of birth:"
-    country = gets.chomp
-    puts "Insert student height:"
-    height = gets.chomp
   end
   #return the array of students
   students
@@ -37,7 +21,7 @@ end
 def print(students)
   count = 0
   until count == students.size
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort hobby:#{students[count][:hobby]}, country of birth:#{students[count][:country]}, height:#{students[count][:height]})"
+    puts "#{students[count][:name]}".ljust(30) + "(#{students[count][:cohort]} cohort)"
     count +=1
   end
 end
