66011b960164764b0d0670db7e100e3bc948666b
TudorTacal <tudortacal@gmail.com>
2016-10-25 10:05:43 +0000

    Exercise 5, added hobbies, country of birth and height

--- directory.rb
+++ directory.rb
@@ -3,13 +3,29 @@ def input_students
   puts "To finish, just hit return twice"
   #create an empty array
   students=[]
+  puts "Insert student name:"
   name = gets.chomp
+  puts "Insert student hobby:"
+  hobby = gets.chomp
+  puts "Insert student country of birth:"
+  country = gets.chomp
+  puts "Insert student height:"
+  height = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
+    
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, hobby: hobby, country: country, height: height, cohort: :november}
     puts "Now we have #{students.count} students"
+    puts "Insert student name:"
     name = gets.chomp
+    break if name.empty?
+    puts "Insert student hobby:"
+    hobby = gets.chomp
+    puts "Insert student country of birth:"
+    country = gets.chomp
+    puts "Insert student height:"
+    height = gets.chomp
   end
   #return the array of students
   students
@@ -21,7 +37,7 @@ end
 def print(students)
   count = 0
   until count == students.size
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort hobby:#{students[count][:hobby]}, country of birth:#{students[count][:country]}, height:#{students[count][:height]})"
     count +=1
   end
 end
