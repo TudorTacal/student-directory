1d4acb92a2d5bc246aeb868ffe37506ec36d80fe
TudorTacal <tudortacal@gmail.com>
2016-10-26 16:28:37 +0000

    Completed exercise 14:1

--- directory.rb
+++ directory.rb
@@ -36,7 +36,7 @@ def load_students(filename = 'students.csv')
   file = File.open(filename, 'r')
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name , cohort: cohort.to_sym}
+    get_students(name,cohort)
   end
   file.close
 end
@@ -71,6 +71,9 @@ def process(selection)
     end
 end
 
+def get_students(student_name,student_cohort)
+  @students << {name: student_name,  cohort: student_cohort.to_sym}
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -81,7 +84,7 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    @students << {name: name,  cohort: cohort}
+    get_students(name,cohort)
     puts @students.count==1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
     name = STDIN.gets.chomp
     break if name ==""
